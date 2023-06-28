177a59770455aafc4d89a5e1a46fd2e9eb7b7c66
Kanjana [OIL] <kanjana.pe@easysunday.com>
2023-06-28 09:46:10 +0700

    finish

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,2 @@
 
 export { default as EventList } from './EventList';
-
-// Button
-// export { default as Button } from './Components2.0/Button/Button';
-
-// // Collapse
-// export { Collapse, Panel } from './Components2.0/Collapse/Collapse';
