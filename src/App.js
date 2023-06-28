dce59eef7a7f39acc93c7e1182e2906117740a15
Kanjana [OIL] <kanjana.pe@easysunday.com>
2023-06-28 07:59:01 +0700

    use axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-// import logo from './logo.svg';
-import { useState } from 'react';
+// import logo from './logo.svg'; // oildel
+import { useEffect, useState } from 'react';
 import './App.css';
 import { EventList } from './components';
-import mockData from './mockData.json'
-function App() {
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState([])
   const [isSortedByEventDateAsc, setIsSortedByEventDateAsc] = useState(true)
+  
 	const sortingOptions = [
 		{ label: 'Ascending', value: true },
 		{ label: 'Descending', value: false }
 	]
+
+  const getData = async () => {
+    const res = await axios.get('https://dev-26t476807dsa2ns.api.raw-labs.com/test')
+    setData(res.data.data)
+  }
+
+  useEffect(()=>{
+    getData()
+  }, [])
+
+  getData()
 
   return (
     <div className="App">
@@ -24,7 +38,7 @@ function App() {
             ))}
           </select>
         </div>
-        <EventList events={mockData.data} isSortedByEventDateAsc={isSortedByEventDateAsc} />
+        <EventList events={data} isSortedByEventDateAsc={isSortedByEventDateAsc} />
       </div>
     </div>
   );
